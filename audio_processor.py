61c81c10f02d13775e2e233856a2d0e4dc369aac
houssamzerrad4-dotcom <houssamzerrad4@gmail.com>
2026-05-02 21:53:32 +0100

    Update audio_processor.py

--- audio_processor.py
+++ audio_processor.py
@@ -3,23 +3,33 @@ import os
 
 
 def extract_audio(video_input, audio_output="temp_audio.mp3"):
-
     # Converts video to mp3 to prepare for AI transcription.
     try:
         if not os.path.exists(video_input):
             print(f"Error: {video_input} not found.")
             print(f"Current working directory: {os.getcwd()}")
             return None
+
         print(f"--- Processing: {video_input} ---")
-      # 2. Use 'with' statement to ensure the file is closed correctly after
+
+        # Use 'with' statement to ensure the file is closed correctly
         with VideoFileClip(video_input) as video:
+
+            if video.audio is None:
+                print("هذا الفيديو لا يحتوي على صوت.")
+                return None
+
             print("Extracting audio track...")
-            # 3. Write the audio file
+
+            # Write the audio file
             video.audio.write_audiofile(audio_output, bitrate="192k")
-        print(f"audio extracted successfully: {audio_output}")
+
+        print(f"Audio extracted successfully: {audio_output}")
         return audio_output
+
     except Exception as e:
-        print(f"error processing {video_input}: {e}")
+        print(f"Error processing {video_input}: {e}")
+        return None
 
 
 if __name__ == "__main__":
